397bce86e0e5d5e834073ecdd410556d3ee41450
liangxin <480102938@qq.com>
2023-05-20 10:50:15 +0800

    fourth commit

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"test/models"
 	"test/routes"
@@ -15,7 +14,4 @@ func main() {
 	routes.ReadyDo(r)
 	//访问端口设置
 	r.Run(":80")
-	fmt.Println("hello git1")
-	fmt.Println("版本2")
-	fmt.Println("test分支，合并了master分支也可以看到")
 }
